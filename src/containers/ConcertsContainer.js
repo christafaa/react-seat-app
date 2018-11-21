19104145799bc81000cfd391613984d5e05b5db5
Chris Alberti <christopherjalberti@gmail.com>
2018-11-21 18:43:36 -0500

    remove default content and create ConcertsContainer

---
+++ src/containers/ConcertsContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ConcertsContainer extends Component {
+  render() {
+    return (
+      <div className="concertsContainer!"></div>
+    )
+  }
+}

e8815eb56f4516c8abe83e13dac8953efba22170
Chris Alberti <christopherjalberti@gmail.com>
2018-11-21 21:53:31 -0500

    create seat and row components

--- src/components/SeatMap.js
+++ src/components/SeatMap.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 export default class SeatMap extends Component {
+
+  createRows = () => {
+    
+  }
+
   render() {
+
     return (
       <div className="nine columns">
-        <p>hi</p>
+        <h5><strong>Title: {this.props.concert.title}</strong></h5>
+        Date: {this.props.concert.date}<br/>
+        Location: {this.props.concert.venue}<br/>
+        {this.createRows()}
       </div>
     )
   }
--- src/containers/ConcertsContainer.js
+++ src/containers/ConcertsContainer.js
@@ -8,8 +8,20 @@ export default class ConcertsContainer extends Component {
     super(props);
 
     this.state = {
+      selectedConcert: {},
       concerts: [
-        {title: "Concert 1"},
+        {
+          title: "Concert 1",
+          date: "December 3, 2018",
+          venue: "Alice Tully Hall",
+          attendees: [],
+          seats: [
+            {number: 1, attendee_id: null}, {number: 2, attendee_id: null}, {number: 3, attendee_id: null}, {number: 4, attendee_id: null}, {number: 5, attendee_id: null},
+            {number: 6, attendee_id: null}, {number: 7, attendee_id: null}, {number: 8, attendee_id: null}, {number: 9, attendee_id: null}, {number: 10, attendee_id: null},
+            {number: 11, attendee_id: null}, {number: 12, attendee_id: null}, {number: 13, attendee_id: null}, {number: 14, attendee_id: null}, {number: 15, attendee_id: null},
+            {number: 16, attendee_id: null}, {number: 17, attendee_id: null}, {number: 18, attendee_id: null}, {number: 19, attendee_id: null}, {number: 20, attendee_id: null}
+          ]
+        },
         {title: "Concert 2"},
         {title: "Concert 3"},
         {title: "Concert 4"},
@@ -36,7 +48,7 @@ export default class ConcertsContainer extends Component {
             {this.renderConcerts()}
           </div>
         </span>
-        <SeatMap />
+        <SeatMap concert={this.state.concerts[0]}/>
       </React.Fragment>
     )
   }

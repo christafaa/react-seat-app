c95e68a018e899584806cdb78ec2103f0fc68cbe
Chris Alberti <christopherjalberti@gmail.com>
2018-11-24 16:35:51 -0500

    create attendees container

---
+++ src/containers/AttendeesContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class AttendeesContainer extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+
+  }
+}
+
+export default connect()(AttendeesContainer)

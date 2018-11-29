add537c7f6fa4db545cc6055c0886be3d0c6aed1
Chris Alberti <christopherjalberti@gmail.com>
2018-11-28 21:39:32 -0500

    move display content from SeatMap to SeatMapHeader component

---
+++ src/components/SeatMapHeader.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Route, NavLink, Switch, withRouter } from 'react-router-dom';
+import AttendeesContainer from '../containers/AttendeesContainer';
+
+
+export default class SeatMapHeader extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.seatmap.id > 0 &&
+          <div className="seven columns">
+            <h5><strong>Title: {this.props.seatmap.title}</strong></h5>
+              <NavLink to={`/concerts/${this.props.seatmap.id}`}>
+                Seat View
+              </NavLink>
+              <span>{" | "}</span>
+              <NavLink to={`/concerts/${this.props.seatmap.id}/attendees`}>
+                List View
+              </NavLink><br/>
+            Date: {this.props.seatmap.date}<br/>
+            Location: {this.props.seatmap.location}<br/>
+          </div>
+        }
+      </React.Fragment>
+    )
+  }
+}
--- src/containers/SeatMapContainer.js
+++ src/containers/SeatMapContainer.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Switch, withRouter } from 'react-router-dom';
 import SeatMap from '../components/SeatMap';
+import SeatMapHeader from '../components/SeatMapHeader';
 import AttendeesContainer from '../containers/AttendeesContainer';
 import { connect } from 'react-redux';
 
 class SeatMapContainer extends Component {
-//move display content to new component
 
   render() {
     return (
       <React.Fragment>
-        {this.props.seatmap.id > 0 &&
-          <div className="seven columns">
-            <h5><strong>Title: {this.props.seatmap.title}</strong></h5>
-              <NavLink to={`/concerts/${this.props.seatmap.id}`}>
-                Seat View
-              </NavLink>
-              <span>{" | "}</span>
-              <NavLink to={`/concerts/${this.props.seatmap.id}/attendees`}>
-                List View
-              </NavLink><br/>
-            Date: {this.props.seatmap.date}<br/>
-            Location: {this.props.seatmap.location}<br/>
-          </div>
-        }
+        <SeatMapHeader seatmap={this.props.seatmap} />
         <Switch>
           <Route exact path="/concerts/:id" render={() => <SeatMap concert={this.props.seatmap} concertId={this.props.seatmap.id} selectedSeats={this.props.selectedSeats}/>}/>
           <Route exact path="/concerts/:id/attendees" render={() => <AttendeesContainer concertId={this.props.seatmap.id}/>}/>
